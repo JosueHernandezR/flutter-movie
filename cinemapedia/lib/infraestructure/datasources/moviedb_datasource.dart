ac16fb92aa8ff8eba9ec2ee8630d1a9c8f3fa382
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2023-09-27 17:08:54 -0600

    Add infraestructure and models for MovieDB 🌝

---
+++ cinemapedia/lib/infraestructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,25 @@
+// Es concebido unicamente para trabajar con themoviedb
+import 'package:dio/dio.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/config/constants/enviroment.dart';
+
+class MovieDBDatasource extends MovieDataSource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Enviroment.movieDBKey,
+        'language': 'es-MX',
+      },
+    ),
+  );
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    final List<Movie> movies = [];
+
+    return movies;
+  }
+}
