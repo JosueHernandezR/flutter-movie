788bd19dd5bbda290162f6322cd193fe321505b9
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2023-10-15 01:10:54 -0600

    Construccion de widgets para las peliculas ✍🏻

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
@@ -27,6 +28,93 @@ class MovieHorizontalListview extends StatelessWidget {
               title: title,
               subtitle: subtitle,
             ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: movies.length,
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return _Slide(movie: movies[index]);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2,
+                        ),
+                      ),
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2,
+              style: textStyle.titleSmall,
+            ),
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.star_half_outlined,
+                color: Colors.yellow.shade800,
+              ),
+              const SizedBox(
+                width: 3,
+              ),
+              Text(
+                movie.voteAverage!.toString(),
+                style: textStyle.bodyMedium?.copyWith(
+                  color: Colors.yellow.shade800,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                movie.popularity.toString(),
+                style: textStyle.bodySmall,
+              )
+            ],
+          )
         ],
       ),
     );
